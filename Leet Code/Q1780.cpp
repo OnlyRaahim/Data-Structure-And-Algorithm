4afe4203ff77d720c61f221504403351b863329d
Raahim_Ali <raahimali290@gmail.com>
2026-07-14 17:49:34 +0500

    commit

---
+++ Leet Code/Q1780.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/submissions/2067359210//
+
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while (n > 0) {
+
+            if (n % 3 == 2)
+                return false;
+
+            n /= 3;
+        }
+
+        return true;
+        
+    }
+};
