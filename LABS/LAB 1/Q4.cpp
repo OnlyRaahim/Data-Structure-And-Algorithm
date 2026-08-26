df4d5b59a9f4f74e63729d6b0998269c95f7d7f2
Raahim_Ali <73752508+hahah420@users.noreply.github.com>
2026-08-26 19:30:30 +0500

    commit

---
+++ LABS/LAB 1/Q4.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int size,int arr[],int target){
+    int left=0;
+    int right=size-1;
+
+    while(left<=right){
+        int mid=(left+right)/2;
+        if(arr[mid]==target) return mid;
+        else if(arr[mid]<target){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+    }
+
+    return -1;
+}
+
+int main(){
+int target;
+int arr[]={-1,0,3,5,9,12};
+int size=6;
+
+cout<<"Enter Target Value: "<<endl;
+cin>>target;
+
+int result=binarySearch(size,arr,target);
+
+if(result != -1){
+    cout<<"Target found at Index: "<<result<<endl;
+}
+else {
+    cout<<result<<endl;
+}
+    return 0;
+}
