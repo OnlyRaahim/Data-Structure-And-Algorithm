b396262344d2d24686177e8c28e8965bb6848e7c
Raahim_Ali <73752508+hahah420@users.noreply.github.com>
2026-08-29 21:49:52 +0500

    commit

---
+++ LABS/LAB 1/Q-28.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/submissions/2124133409/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int s1=haystack.size();
+        int s2=needle.size();
+
+        for(int i=0; i<=s1-s2; i++){
+            for (int j=0; j<s2; j++){
+                if(haystack[i+j] != needle[j]){
+                    break;
+                }
+
+                if(j==s2-1) return i;
+            }
+        }
+        return -1;
+    }
+};
