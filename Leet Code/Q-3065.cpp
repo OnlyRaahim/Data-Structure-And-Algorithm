b8832947048b114d35c8efcbb24c5193253ac628
Raahim_Ali <73752508+hahah420@users.noreply.github.com>
2026-09-02 22:36:44 +0500

    commit

---
+++ Leet Code/Q-3065.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-i/submissions/2128773052/
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int count=0;
+        
+
+        for(int num: nums){
+            if(num<k){
+                count++;
+            }
+
+        }
+        return count;
+    }
+};
