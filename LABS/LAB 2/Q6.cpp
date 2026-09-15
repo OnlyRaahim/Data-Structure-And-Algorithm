fb1eff8d64b1b929aaeed64160d7474f96798489
Raahim_Ali <raahimali290@gmail.com>
2026-09-16 02:13:37 +0500

    commit

---
+++ LABS/LAB 2/Q6.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Stack{
+    public:
+    struct Node{
+        int data;
+        Node* next;
+
+        Node(int value){
+            data=value;
+            next=NULL;
+        }
+    };
+
+
+    Node* top;
+
+    Stack(){
+        top=NULL;
+    }
+
+    void push(int value){
+        Node* newNode=new Node(value);
+        newNode->next=top;
+        top=newNode;
+    }
+
+    void pop(){
+        if (top==NULL){
+            cout<<"Stack Underflow!!"<<endl;
+            return;
+        }
+
+        Node* temp = top;
+        top=top->next;
+        delete temp;
+    }
+
+    int peek(){
+        if(top==NULL){
+            cout<<"Stack Is Empty!!"<<endl;
+            return -1;
+        }
+        return top->data;
+    }
+
+    bool isEmpty(){
+        return top==NULL;
+    }
+
+    void display(){
+        Node* temp=top;
+
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+
+    ~Stack(){
+        while(top!=NULL){
+            pop();
+        }
+    }
+
+};
+
+int main(){
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    
+
+    cout<<"Before Popping "<<endl;
+    s.display();
+
+
+    s.pop();
+    cout<<"After popping"<<endl;
+    s.display();
+
+
+}
