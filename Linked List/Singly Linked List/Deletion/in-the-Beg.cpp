911c749fa06c34a5c3cb44bc7f9580eafe2663ac
Raahim_Ali <73752508+hahah420@users.noreply.github.com>
2026-09-18 01:58:01 +0500

    commit

---
+++ Linked List/Singly Linked List/Deletion/in-the-Beg.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int value){
+        data=value;
+        next=nullptr;
+    }
+};
+
+class LinkedList{
+    public:
+    Node *head;
+
+    LinkedList(){
+        head=NULL;
+    }
+
+    void insert(int value){
+        Node* newNode= new Node(value);
+        
+        if(head==NULL){
+            head=newNode;
+            return;
+        }
+
+        newNode->next=head;
+        head=newNode;
+
+    }
+
+    void deleteFromBeg(){
+        Node* temp = head;
+        if(head==NULL){
+            cout<<"List Is Empty!! "<<endl;
+            return;
+        }
+        else{
+            Node* temp=head;
+            head=head->next;
+            delete temp;
+        }
+    }
+
+    void display(){
+        Node* temp=head;
+
+        while(temp!=NULL){
+            cout<<temp->data<<"->";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+};
+
+int main (){
+    LinkedList list;
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+    list.insert(40);
+
+    list.display();
+    list.deleteFromBeg();
+    list.display();
+
+    
+    return 0;
+}
